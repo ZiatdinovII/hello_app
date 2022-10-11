1bea1b31f002a2b287e43529981c7d11b3cd6bc7
ZiatdinovII <i.kamalutdinovG@yandex.ru>
2022-10-11 11:34:37 +0300

    hola, mundo!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     
         def hello
-            render plain: "hello, world!"
+            # render plain: "hello, world!"
+            render plain: "hola, mundo!"
         end
 end
